0ddd7714a6390524c5a4871ad4ba7605087504d0
daehan-lim <penjan.eng@gmail.com>
2025-05-16 16:01:23 +0900

    feat: 홈 화면 및 피드 탭 임시 레이아웃 추가

--- lib/app/constants/app_styles.dart
+++ lib/app/constants/app_styles.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 abstract class AppStyles {
   //dimensions
   static const double mediumDimension = 24;
--- lib/presentation/pages/home/home_view_model.dart
+++ lib/presentation/pages/home/home_view_model.dart
@@ -1,6 +1,16 @@
-// class HomeViewModel extends Notifier<HomeState> {
-// }
-//
-// final homeViewModelProvider = NotifierProvider<HomeViewModel, HomeState>(
-//   HomeViewModel.new,
-// );
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeViewModel extends AutoDisposeNotifier<int> {
+  @override
+  int build() {
+    return 0;
+  }
+
+  void onIndexChanged(int newIndex) {
+    state = newIndex;
+  }
+}
+
+final homeViewModelProvider = AutoDisposeNotifierProvider<HomeViewModel, int>(() {
+  return HomeViewModel();
+});
