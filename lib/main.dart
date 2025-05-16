5dbf6b803807e5a776cf6439c5bd65141cdb2d4a
daehan-lim <penjan.eng@gmail.com>
2025-05-16 16:39:29 +0900

    feat: Firebase 설치 및 연결

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,6 +7,9 @@ import 'app/constants/app_constants.dart';
 import 'app/theme.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   await dotenv.load(fileName: '.env');
   runApp(const ProviderScope(child: MyApp()));
 }
