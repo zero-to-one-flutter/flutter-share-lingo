f9ad1a536baa6113b46fc5728637caa74f2372c0
ghghtls <ghghtls2@gmail.com>
2025-05-16 21:36:48 +0900

    fix: 태그 카메라 아이콘 여백

--- lib/presentation/pages/home/tabs/write/widgets/tag_row_button.dart
+++ lib/presentation/pages/home/tabs/write/widgets/tag_row_button.dart
@@ -6,53 +6,56 @@ class TagRowButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        // 왼쪽: 태그 추가 버튼
-        GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SelectTagPage()),
-            );
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12),
-            height: 32,
-            decoration: BoxDecoration(
-              color: const Color(0xFFA9A9A9),
-              borderRadius: BorderRadius.circular(20),
-            ),
-            alignment: Alignment.center,
-            child: const Text(
-              '태그 추가',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 15,
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w400,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          // 왼쪽: 태그 추가 버튼
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SelectTagPage()),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              height: 32,
+              decoration: BoxDecoration(
+                color: const Color(0xFFA9A9A9),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              alignment: Alignment.center,
+              child: const Text(
+                '태그 추가',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 15,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
           ),
-        ),
 
-        // 오른쪽: 카메라 버튼
-        GestureDetector(
-          onTap: () {
-            // TODO: 사진 선택 기능
-          },
-          child: Container(
-            width: 48,
-            height: 48,
-            decoration: const BoxDecoration(
-              color: Color(0xFF007AFF),
-              shape: BoxShape.circle,
+          // 오른쪽: 카메라 버튼
+          GestureDetector(
+            onTap: () {
+              // TODO: 사진 선택 기능
+            },
+            child: Container(
+              width: 48,
+              height: 48,
+              decoration: const BoxDecoration(
+                color: Color(0xFF007AFF),
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(Icons.camera_alt, color: Colors.white),
             ),
-            child: const Icon(Icons.camera_alt, color: Colors.white),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
