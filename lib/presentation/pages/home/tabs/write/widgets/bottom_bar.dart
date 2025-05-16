ca6d532d7e5b359dfa1d55ce470cc68679ed91bb
ghghtls <ghghtls2@gmail.com>
2025-05-16 18:33:02 +0900

    feat:포스트 스낵바 추가

---
+++ lib/presentation/pages/home/tabs/write/widgets/bottom_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'cancel_button.dart';
+import 'submit_button.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 393,
+      height: 56,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            CancelButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            SubmitButton(
+              onPressed: () {
+                // TODO: 게시 로직 작성
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(const SnackBar(content: Text("게시되었습니다.")));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
