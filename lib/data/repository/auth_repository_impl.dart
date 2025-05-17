d3039d8e2c91a4f840c653d4ede187df36f74c27
daehan-lim <penjan.eng@gmail.com>
2025-05-17 14:49:11 +0900

    feat: implement Google sign-in and Firestore user save in repository
    Added AuthRepositoryImpl with methods to:
    - Sign in and sign out using Google
    - Save authenticated user data to Firestore
    - Added providers needed for AuthRepository including all data sources and dependencies for them

---
+++ lib/data/repository/auth_repository_impl.dart
@@ -0,0 +1,53 @@
+import '../../domain/entity/app_user.dart';
+import '../../domain/repository/auth_repository.dart';
+import '../data_source/firebase_auth_data_source.dart';
+import '../data_source/google_sign_in_data_source.dart';
+import '../data_source/user_data_source.dart';
+import '../dto/user_dto.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final GoogleSignInDataSource _googleSignIn;
+  final FirebaseAuthDataSource _firebaseAuth;
+  final UserDataSource userDataSource;
+
+  AuthRepositoryImpl(
+    this._googleSignIn,
+    this._firebaseAuth,
+    this.userDataSource,
+  );
+
+  @override
+  Future<AppUser?> signInWithGoogle() async {
+    final googleAuth = await _googleSignIn.signIn();
+    if (googleAuth == null) return null;
+
+    final firebaseUser = await _firebaseAuth.signInWithGoogle(googleAuth);
+    if (firebaseUser == null) return null;
+
+    final partialUser = AppUser(
+      id: firebaseUser.uid,
+      name: firebaseUser.displayName ?? 'User',
+      profileImage: firebaseUser.photoURL,
+      email: firebaseUser.email,
+    );
+
+    final fullUserDto = await userDataSource.getUserById(partialUser.id);
+    if (fullUserDto != null) { // User exists in backend
+      return fullUserDto.toEntity(); // Return existing user in backend
+    } else { // User is new sign up. Add to backend
+      await userDataSource.saveUser(UserDto.fromEntity(partialUser));
+      return partialUser; // Return newly added user
+    }
+  }
+
+  @override
+  Future<void> signOut() async {
+    await _googleSignIn.signOut();
+    await _firebaseAuth.signOut();
+  }
+
+  @override
+  Stream<String?> authStateChanges() {
+    return _firebaseAuth.authStateChanges().map((user) => user?.uid);
+  }
+}
