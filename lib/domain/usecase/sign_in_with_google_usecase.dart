fdb03d6a96fc29c8a3dcd6626894b138b2b63759
daehan-lim <penjan.eng@gmail.com>
2025-05-17 15:39:05 +0900

    feat: Add sign_in_with_google_usecase.dart

---
+++ lib/domain/usecase/sign_in_with_google_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../core/providers/data_providers.dart';
+import '../entity/app_user.dart';
+import '../repository/auth_repository.dart';
+
+class SignInWithGoogleUseCase {
+  final AuthRepository _repository;
+
+  SignInWithGoogleUseCase(this._repository);
+
+  Future<AppUser?> execute() => _repository.signInWithGoogle();
+}
+
+final signInWithGoogleUseCaseProvider = Provider(
+      (ref) => SignInWithGoogleUseCase(ref.watch(authRepositoryProvider)),
+);
