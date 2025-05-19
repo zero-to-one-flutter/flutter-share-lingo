76d56856cbd12c6e57d7672e82bdc78add8fc552
ghghtls <ghghtls2@gmail.com>
2025-05-19 13:02:31 +0900

    feat:테스트편하게 코드 변경

--- lib/data/data_source/post_remote_data_source.dart
+++ lib/data/data_source/post_remote_data_source.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../dto/post_dto.dart';
+import 'package:share_lingo/data/dto/post_dto.dart';
 
 class PostRemoteDataSource {
-  final _firestore = FirebaseFirestore.instance;
-  final String _collection = 'posts';
+  final FirebaseFirestore firestore;
+
+  PostRemoteDataSource({FirebaseFirestore? firestore})
+    : firestore = firestore ?? FirebaseFirestore.instance;
 
   Future<void> createPost(PostDto postDto) async {
-    await _firestore.collection(_collection).add(postDto.toMap());
+    await firestore.collection('posts').add(postDto.toMap());
   }
 }
