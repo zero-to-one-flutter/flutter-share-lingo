1a00d66ba2c5fa9ab28511fae12d966550262485
daehan-lim <penjan.eng@gmail.com>
2025-05-20 15:45:30 +0900

    chore(user_validator): Rephrase validation message

--- lib/core/ui_validators/user_validator.dart
+++ lib/core/ui_validators/user_validator.dart
@@ -1,7 +1,7 @@
 class UserValidator {
   static String? validateName(String? name) {
     if (name == null || name.trim().isEmpty) return '이름을 입력해 주세요';
-    if (name.trim().length < 2) return '2글자 이상 입력해야 해요';
+    if (name.trim().length < 2) return '1글자 이상 입력해 주세요';
     return null;
   }
 
