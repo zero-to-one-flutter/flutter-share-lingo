aecc265809aabc28e61950816472df2fb7e390e6
daehan-lim <penjan.eng@gmail.com>
2025-05-22 19:04:00 +0900

    refactor: Remove unused import

--- lib/presentation/pages/home/widgets/post_item.dart
+++ lib/presentation/pages/home/widgets/post_item.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
