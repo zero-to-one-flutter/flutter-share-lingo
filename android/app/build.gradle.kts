bf1adeec968d00ef389eb84278003f10a4ed82ab
daehan-lim <penjan.eng@gmail.com>
2025-05-25 02:35:46 +0900

    chore: Add proguard-rules.pro for play store update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -54,6 +54,10 @@ android {
 
     buildTypes {
         release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                file("proguard-rules.pro")
+            )
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
